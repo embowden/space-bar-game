d123bbcc1964ac24602531d5931db3d7920ff241
Emma Bowden <97846259+embowden@users.noreply.github.com>
2023-03-21 13:21:41 +0000

    update api link

--- client/src/api/endpoints.js
+++ client/src/api/endpoints.js
@@ -1,11 +1,11 @@
-export const API_CODE = `http://localhost:8080/mdn/code`;
+// export const API_CODE = `http://localhost:8080/mdn/code`;
 
-export const API_SUMMARY = `http://localhost:8080/mdn/summary`;
+// export const API_SUMMARY = `http://localhost:8080/mdn/summary`;
 
-export const API_LINKS = `http://localhost:8080/mdn/links`;
+// export const API_LINKS = `http://localhost:8080/mdn/links`;
 
-// export const API_CODE = `https://spacebar-server.herokuapp.com/mdn/code`;
+export const API_CODE = `https://spacebar-server.onrender.com/mdn/code`;
 
-// export const API_SUMMARY = `https://spacebar-server.herokuapp.com/mdn/summary`;
+export const API_SUMMARY = `https://spacebar-server.onrender.com/mdn/summary`;
 
-// export const API_LINKS = `https://spacebar-server.herokuapp.com/mdn/links`;
+export const API_LINKS = `https://spacebar-server.onrender.com/mdn/links`;
